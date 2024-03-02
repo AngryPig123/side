e6af0052c93d7cf19f4a342977759f0b3e13f8a2
Angrypig123 <shguddnr3@naver.com>
2024-03-02 13:54:14 +0900

    project init

---
+++ src/main/java/org/spring/oauth2/email_validate_register/EmailValidateRegisterApplication.java
@@ -0,0 +1,13 @@
+package org.spring.oauth2.email_validate_register;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailValidateRegisterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailValidateRegisterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/spring/oauth2/email_validate_register/EmailValidateRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spring.oauth2.email_validate_register;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailValidateRegisterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
